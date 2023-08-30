3b4d568b4f816c8a4b6d2f9e79d2d2a61afe9a71
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-08-30 13:47:35 +0300

    done

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'purchase_price', 'category',)
+    list_filter = ('category',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    # created_at = models.CharField(max_length=100, **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    purchase_price = models.IntegerField(verbose_name='Цена за покупку')
+    date_of_creation = models.DateField(max_length=100, verbose_name='Дата создания')
+    last_modified_date = models.DateField(max_length=100, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
