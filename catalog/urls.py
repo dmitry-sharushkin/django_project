a535e44467d479a7d3f4bd71215e8d6d9f7d21ef
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-09-21 21:21:57 +0300

    done 2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, re_path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, categories, products
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    re_path(r'^contacts', contacts),
+    path('', home, name='home'),
+    re_path(r'^contacts', contacts, name='contacts'),
+    re_path(r'^categories', categories, name='categories'),
+    path('<int:pk>/products/', products, name='products'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 def home(request):
     return render(request, 'catalog/home.html')
@@ -7,3 +9,23 @@ def home(request):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Категории товаров'
+
+    }
+    return render(request, 'catalog/categories.html', context)
+
+
+def products(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category=pk),
+        'title': f'Продукты {category_item.name}'
+
+    }
+    return render(request, 'catalog/products.html', context)
+
